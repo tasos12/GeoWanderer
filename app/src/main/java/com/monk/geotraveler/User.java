a3a67ec46802d57e21a1356eb81f04241e7b315c
Γκάγκας Αναστάσιος <tasosggps@gmail.com>
2019-01-11 12:36:43 +0200

    Added database and User class

---
+++ app/src/main/java/com/monk/geotraveler/User.java
@@ -0,0 +1,89 @@
+package com.monk.geotraveler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+
+    private float mDistanceMaxTodayValue = 0;
+    private float mDistanceTotalValue = 0;
+    private float mDistanceMaxValue = 0;
+    private double mHomeLatitude = -1;
+    private double mHomeLongitude = -1;
+
+    public User(){
+
+    }
+
+    public User(float maxDistanceToday , float maxDistance, float totalDistance){
+        mDistanceMaxTodayValue = maxDistanceToday;
+        mDistanceTotalValue = totalDistance;
+        mDistanceMaxValue = maxDistance;
+    }
+
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+
+        result.put("maxDistance", mDistanceMaxValue);
+        result.put("maxDistanceToday", mDistanceMaxTodayValue);
+        result.put("totalDistance", mDistanceTotalValue);
+
+        return result;
+    }
+
+    public void setHomeLocation(double latitude, double longitude){
+        setHomeLongitude(longitude);
+        setHomeLatitude(latitude);
+    }
+
+    public void setUserInfo(float maxDistanceToday , float maxDistance, float totalDistance){
+        mDistanceMaxTodayValue = maxDistanceToday;
+        mDistanceTotalValue = totalDistance;
+        mDistanceMaxValue = maxDistance;
+    }
+
+    public float getDistanceMaxTodayValue() {
+        return mDistanceMaxTodayValue;
+    }
+
+    public void setDistanceMaxTodayValue(float mDistanceMaxTodayValue) {
+        this.mDistanceMaxTodayValue = mDistanceMaxTodayValue;
+    }
+
+    public float getDistanceTotalValue() {
+        return mDistanceTotalValue;
+    }
+
+    public void setDistanceTotalValue(float mDistanceTotalValue) {
+        this.mDistanceTotalValue = mDistanceTotalValue;
+    }
+
+    public float getDistanceMaxValue() {
+        return mDistanceMaxValue;
+    }
+
+    public void setDistanceMaxValue(float mDistanceMaxValue) {
+        this.mDistanceMaxValue = mDistanceMaxValue;
+    }
+
+    public double getHomeLatitude() {
+        return mHomeLatitude;
+    }
+
+    public void setHomeLatitude(double mHomeLatitude) {
+        this.mHomeLatitude = mHomeLatitude;
+    }
+
+    public double getHomeLongitude() {
+        return mHomeLongitude;
+    }
+
+    public void setHomeLongitude(double homeLongitude) {
+        this.mHomeLongitude = homeLongitude;
+    }
+
+    public boolean isHomeLocationSet() {
+        if(mHomeLongitude != -1 && mHomeLatitude != -1) return true;
+        else return false;
+    }
+}
