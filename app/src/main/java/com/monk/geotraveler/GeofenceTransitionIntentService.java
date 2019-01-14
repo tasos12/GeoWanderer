6719f2e274bb520929f71c6209cfd3c2623806f0
Γκάγκας Αναστάσιος <tasosggps@gmail.com>
2019-01-14 13:20:01 +0200

    Adding window animation an Geofence Classes

---
+++ app/src/main/java/com/monk/geotraveler/GeofenceTransitionIntentService.java
@@ -0,0 +1,129 @@
+package com.monk.geotraveler;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeofenceTransitionIntentService extends IntentService {
+
+    private final String TAG = "GeofenceIntent";
+    private String description;
+
+    public GeofenceTransitionIntentService(){
+        super("Display Notification");
+    }
+
+    protected void onHandleIntent(Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            Log.e(TAG, "Geofencing Error: " + geofencingEvent.toString());
+            return;
+        }
+
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+        description = intent.getStringExtra("description");
+        Log.d(TAG, "Geofence intent sent");
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL) {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            // Get the transition details as a String.
+            String geofenceTransitionDetails = getGeofenceTransitionDetails(
+                    this,
+                    geofenceTransition,
+                    triggeringGeofences
+            );
+
+            Log.d(TAG, "Geofence Triggered: " + geofenceTransitionDetails);
+
+            // Send notification and log the transition details.
+            sendNotification(geofenceTransitionDetails);
+            Log.i(TAG, geofenceTransitionDetails);
+        } else {
+            // Log the error.
+            Log.e(TAG, "Geofence error");
+        }
+    }
+
+    private String getGeofenceTransitionDetails(Context context, int geofenceTransition, List<Geofence> triggeringGeofences) {
+
+        String geofenceTransitionString = getTransitionString(geofenceTransition);
+
+        // Get the Ids of each geofence that was triggered.
+        ArrayList triggeringGeofencesIdsList = new ArrayList();
+        for (Geofence geofence : triggeringGeofences) {
+            triggeringGeofencesIdsList.add(geofence.getRequestId());
+        }
+        String triggeringGeofencesIdsString = TextUtils.join(", ", triggeringGeofencesIdsList);
+
+        return geofenceTransitionString + ": " + triggeringGeofencesIdsString;
+    }
+
+    private void sendNotification(String notificationDetails) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(getApplicationContext(), LoginActivity.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainScreen.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        String details[] = notificationDetails.split(":");
+
+        // Get a notification builder that's compatible with platform versions >= 4
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        // Define the notification settings.
+        builder.setContentTitle(details[1].trim())
+                .setContentText(description)
+                .setContentIntent(notificationPendingIntent)
+                .setSmallIcon(R.drawable.ic_menu_share);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
+    }
+
+    private String getTransitionString(int transitionType) {
+        switch (transitionType) {
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                return "transition - enter";
+            case Geofence.GEOFENCE_TRANSITION_DWELL:
+                return "transition - dwell";
+            default:
+                return "unknown transition";
+        }
+    }
+}
---
+++ app/src/main/java/com/monk/geotraveler/MemoryFence.java
@@ -0,0 +1,51 @@
+package com.monk.geotraveler;
+
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MemoryFence {
+
+    private final String TAG = "MemoryFence";
+
+    private Geofence mGeofence;
+    private String id, description;
+    private double latitude, longitude;
+
+    public MemoryFence(String id, String description, LatLng point){
+        this.id = id;
+        this.description = description;
+        latitude = point.latitude;
+        longitude = point.longitude;
+        mGeofence = new Geofence.Builder()
+                .setRequestId(id)
+                .setCircularRegion(point.latitude, point.longitude, 50)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_DWELL)
+                .setLoiteringDelay(1000)
+                .build();
+        Log.d(TAG, "MemoryFence: ");
+    }
+
+    public Geofence getGeofence(){
+        return mGeofence;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
